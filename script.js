4ea6b4fd63a2c52d1f2555acdb1ad3527019a7b9
MaiconDz <maiconzefe@gmail.com>
2025-02-26 13:09:37 -0300

    Adicionando sessão de forms

--- script.js
+++ script.js
@@ -17,5 +17,69 @@ document.addEventListener("DOMContentLoaded", function() {
         history.replaceState(null, null, window.location.pathname + window.location.search);
         window.scrollTo(0, 0);
     }
+
+    const telefoneInput = document.getElementById('telefone');
+
+    // Define duas máscaras: uma para fixo e outra para celular
+    const maskOptions = {
+      mask: [
+        { mask: '(00) 0000-0000' },  // para telefone fixo
+        { mask: '(00) 00000-0000' }  // para celular
+      ],
+      // Escolhe a máscara com base na quantidade de dígitos digitados
+      dispatch: function (appended, dynamicMasked) {
+        const number = (dynamicMasked.value + appended).replace(/\D/g, '');
+        return number.length > 10 ? dynamicMasked.compiledMasks[1] : dynamicMasked.compiledMasks[0];
+      }
+    };
+
+    IMask(telefoneInput, maskOptions);
+
+    document.addEventListener("DOMContentLoaded", function() {
+        const form = document.getElementById("form");
+      
+        form.addEventListener("submit", function(e) {
+          e.preventDefault(); // Impede o envio padrão do formulário
+      
+          // Captura os valores dos campos do formulário
+          const nome = document.getElementById("nome").value;
+          const email = document.getElementById("email").value;
+          const telefone = document.getElementById("telefone").value;
+          const faturamento = document.getElementById("faturamento").value;
+      
+          // Cria o objeto payload com os dados a serem enviados
+          const payload = {
+            nome: nome,
+            email: email,
+            telefone: telefone,
+            faturamento: faturamento
+          };
+      
+          // Envia os dados para o n8n via fetch
+          fetch("https://backend.noblecompany.digital/webhook-test/forms_lp", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify(payload)
+          })
+            .then(response => {
+              if (response.ok) {
+                return response.json(); // ou response.text(), conforme o retorno do seu webhook
+              } else {
+                throw new Error("Erro ao enviar os dados");
+              }
+            })
+            .then(data => {
+              console.log("Dados enviados com sucesso:", data);
+              alert("Obrigado! Em breve entraremos em contato.");
+              form.reset();
+            })
+            .catch(error => {
+              console.error("Erro:", error);
+              alert("Ocorreu um erro ao enviar seus dados. Tente novamente.");
+            });
+        });
+    });
 });
 
